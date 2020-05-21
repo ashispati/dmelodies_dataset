c588948f428db279ef24eb6c5a2db5eaf828059c
Ashis Pati <ashis.pati@gatech.edu>
2020-05-20 21:08:58 -0400

    change file names

--- constants_file_names.py
+++ constants_file_names.py
@@ -4,8 +4,8 @@ Do NOT change
 """
 
 RAW_DATA_FOLDER = 'raw_data'  # will contain the raw data in .musicxml or .mid formats
-DATASETS_FOLDER = 'datasets'  # will contain the index dictionary file and dataset in .npz format
+DATASETS_FOLDER = 'dataset'  # will contain the index dictionary file and dataset in .npz format
 
-LATENT_INFO_CSV = 'dmelodies_dataset_latent_info.csv'  # csv containing latent values for all data points
-NPZ_DATASET = 'dmelodies_dataset.npz'  # dataset stored as numpy nd-arrays
-INDEX_DICT_FILE = 'index_dicts.txt'  # text file storing dictionary mapping indices to music21 note names
+LATENT_INFO_CSV = 'dMelodies_dataset_latent_info.csv'  # csv containing latent values for all data points
+NPZ_DATASET = 'dMelodies_dataset.npz'  # dataset stored as numpy nd-arrays
+INDEX_DICT_FILE = 'index_dicts.json'  # text file storing dictionary mapping indices to music21 note names
