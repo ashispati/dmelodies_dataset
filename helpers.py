e31b8a2d22408e4a3bc33290d881ae13c501bd6b
Ashis Pati <ashis.pati@gatech.edu>
2020-05-20 21:09:19 -0400

    add helpers

---
+++ helpers.py
@@ -0,0 +1,260 @@
+"""
+Helper functions to create melodies and music21 score objects
+"""
+import os
+from fractions import Fraction
+from itertools import product
+import pandas as pd
+from tqdm import tqdm
+from typing import Union
+
+import music21
+from music21 import note
+
+from constants_file_names import *
+from constants_latent_factors import *
+
+
+SLUR_SYMBOL = '__'
+TICK_VALUES = [
+    0,
+    Fraction(1, 2),
+]
+
+
+def create_latent_info_df():
+    """
+    Creates and returns the dataframe object containing the latent factors information
+
+    Returns:
+         pandas data-frame object
+    """
+    tonic_list = []
+    octave_list = []
+    scale_list = []
+    rhy1_list = []
+    rhy2_list = []
+    dir1_list = []
+    dir2_list = []
+    dir3_list = []
+    dir4_list = []
+
+    all_combinations = product(
+        TONIC_DICT.keys(),
+        OCTAVE_DICT.keys(),
+        SCALE_DICT.keys(),
+        RHYTHM_DICT.keys(),
+        RHYTHM_DICT.keys(),
+        ARP_DICT.keys(),
+        ARP_DICT.keys(),
+        ARP_DICT.keys(),
+        ARP_DICT.keys()
+    )
+    for params in tqdm(all_combinations):
+        tonic_list.append(TONIC_DICT[params[0]])
+        octave_list.append(OCTAVE_DICT[params[1]])
+        scale_list.append(SCALE_DICT[params[2]])
+        rhy1_list.append(params[3])
+        rhy2_list.append(params[4])
+        dir1_list.append(ARP_DICT[params[5]])
+        dir2_list.append(ARP_DICT[params[6]])
+        dir3_list.append(ARP_DICT[params[7]])
+        dir4_list.append(ARP_DICT[params[8]])
+    d = {
+        'tonic': tonic_list,
+        'octave': octave_list,
+        'scale': scale_list,
+        'rhythm_bar1': rhy1_list,
+        'rhythm_bar2': rhy2_list,
+        'arp_chord1': dir1_list,
+        'arp_chord2': dir2_list,
+        'arp_chord3': dir3_list,
+        'arp_chord4': dir4_list
+    }
+    latent_df = pd.DataFrame(data=d)
+    return latent_df
+
+
+def get_latent_info():
+    """
+    Reads the latent factors info from stored LATENT_INFO_CSV (see constants_file_names.py) file.
+    If file doesn't exist, creates and saves it
+
+    Returns:
+        pandas data-frame object
+    """
+    if os.path.exists(LATENT_INFO_CSV):
+        latent_df = pd.read_csv('dmelodies_dataset_latent_info.csv', index_col=0)
+    else:
+        latent_df = create_latent_info_df()
+        latent_df.to_csv(path_or_buf=LATENT_INFO_CSV)
+    return latent_df
+
+
+def get_midi_pitch_list(
+        tonic: str,
+        octave: int,
+        mode: str,
+        arp_dir1: str,
+        arp_dir2: str,
+        arp_dir3: str,
+        arp_dir4: str
+) -> list:
+    """
+    Create the sequence of midi pitch values. Refer constants_latent_factors.py for details regarding allowed arg values
+    Args:
+        tonic: str, specifies the pitch class of the root note (C, C#, ...., through B)
+        octave: int, specifies of the octave number (4, 5, 6) of the root note
+        mode: str, specifies the scale (major, minor, blues etc.)
+        arp_dir1: str, 'up' or 'down', specifies the arpeggiation direction of Chord 1
+        arp_dir2: str, 'up' or 'down', specifies the arpeggiation direction of Chord 2
+        arp_dir3: str, 'up' or 'down', specifies the arpeggiation direction of Chord 3
+        arp_dir4: str, 'up' or 'down', specifies the arpeggiation direction of Chord 4
+    Returns:
+        list of MIDI notes corresponding to the melody defined based on the input arguments
+    """
+    root_pitch = music21.pitch.Pitch(tonic + str(octave)).midi
+    pitch_seq = []
+    dir_seq = [arp_dir1, arp_dir2, arp_dir3, arp_dir4]
+    for index, chord in enumerate(CHORD_DICT.keys()):
+        seq = CHORD_DICT[chord]
+        if dir_seq[index] == 'down':
+            seq = seq[::-1]
+        for s in seq:
+            midi_pitch = root_pitch + SCALE_DICT[mode][s]
+            pitch_seq.append(midi_pitch)
+    return pitch_seq
+
+
+def create_m21_melody(
+        tonic: str,
+        octave: int,
+        mode: str,
+        rhythm_bar1: int,
+        rhythm_bar2: int,
+        arp_dir1: str,
+        arp_dir2: str,
+        arp_dir3: str,
+        arp_dir4: str
+) -> music21.stream.Score:
+    """
+    Creates the 2-bar melody in music21 score format
+    Args:
+        tonic: str, specifies the pitch class of the root note (C, C#, ...., through B)
+        octave: int, specifies of the octave number (4, 5, 6) of the root note
+        mode: str, specifies the scale (major, minor, blues etc.)
+        rhythm_bar1: int, specified the rhythm for Bar 1
+        rhythm_bar2: int, specified the rhythm for Bar 2
+        arp_dir1: str, 'up' or 'down', specifies the arpergiation direction of Chord 1
+        arp_dir2: str, 'up' or 'down', specifies the arpeggiation direction of Chord 2
+        arp_dir3: str, 'up' or 'down', specifies the arpeggiation direction of Chord 3
+        arp_dir4: str, 'up' or 'down', specifies the arpeggiation direction of Chord 4
+
+    Returns:
+        music21 score object containing the score
+    """
+    score = music21.stream.Score()
+    part = music21.stream.Part()
+    dur = 0.0
+    rhy1 = RHYTHM_DICT[rhythm_bar1]
+    rhy2 = RHYTHM_DICT[rhythm_bar2]
+    if sum(rhy1) != 6:
+        raise(ValueError, f'Invalid rhythm: {rhy1}')
+    if sum(rhy2) != 6:
+        raise(ValueError, f'Invalid rhythm: {rhy2}')
+    midi_pitch_seq = get_midi_pitch_list(tonic, octave, mode, arp_dir1, arp_dir2, arp_dir3, arp_dir4)
+    curr_note_num = 0
+    for rhy in [rhy1, rhy2]:
+        for onset in rhy:
+            if onset == 1:
+                f = music21.note.Note()
+                f.pitch.midi = midi_pitch_seq[curr_note_num]
+                f.duration = music21.duration.Duration('eighth')
+                curr_note_num += 1
+            else:
+                f = music21.note.Rest()
+                f.duration = music21.duration.Duration('eighth')
+            part.insert(dur, f)
+            dur += music21.duration.Duration('eighth').quarterLength
+
+    score.insert(part)
+    return score
+
+
+def compute_tick_durations(tick_values):
+    """
+    Computes the tick durations
+    Args:
+        tick_values: list of allowed tick values
+    """
+    diff = [n - p
+            for n, p in zip(tick_values[1:], tick_values[:-1])]
+    diff = diff + [1 - tick_values[-1]]
+    return diff
+
+
+def get_notes(score: music21.stream.Score) -> list:
+    """
+    Returns the notes from the music21 score object
+    Args:
+        score: music21 score object
+    Returns:
+        list, of music21 note objects
+    """
+    notes = score.parts[0].flat.notesAndRests
+    notes = [n for n in notes if not isinstance(n, music21.harmony.ChordSymbol)]
+    return notes
+
+
+def is_score_on_ticks(score: music21.stream.Score, tick_values) -> bool:
+    """
+    Checks if the notes in a score are on ticks
+    Args:
+        score: music21 score object
+        tick_values: list of allowed tick values
+    """
+    notes = get_notes(score)
+    eps = 1e-5
+    for n in notes:
+        _, d = divmod(n.offset, 1)
+        flag = False
+        for tick_value in tick_values:
+            if tick_value - eps < d < tick_value + eps:
+                flag = True
+        if not flag:
+            return False
+    return True
+
+
+def standard_name(note_or_rest) -> str:
+    """
+    Converts music21 note objects to string
+    Args:
+        note_or_rest: music21 note.Note or note.Rest object
+
+    Returns:
+        str,
+    """
+    if isinstance(note_or_rest, music21.note.Note):
+        return note_or_rest.nameWithOctave
+    elif isinstance(note_or_rest, music21.note.Rest):
+        return note_or_rest.name
+    else:
+        raise ValueError("Invalid input. Should be a music21.note.Note or music21.note.Rest object ")
+
+
+def standard_note(note_or_rest_string) -> Union[music21.note.Note, music21.note.Rest]:
+    """
+    Converts str to music21 note.Note or note.Rest object
+    Args:
+        note_or_rest_string:
+
+    Returns:
+        music21 note.Note or note.Rest object
+    """
+    if note_or_rest_string == 'rest':
+        return note.Rest()
+    elif note_or_rest_string == SLUR_SYMBOL:
+        return note.Rest()
+    else:
+        return note.Note(note_or_rest_string)
