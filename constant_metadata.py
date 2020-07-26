78bad63e048095825057a9afb1bb21e35f2ae1b3
Ashis Pati <ashis.pati@gatech.edu>
2020-07-26 15:58:09 -0400

    add jupyter notebook with examples

---
+++ constant_metadata.py
@@ -0,0 +1,12 @@
+"""
+File containing some constants for the dataset Metadata
+Do NOT change
+"""
+
+TITLE = 'dMelodies dataset'
+DESCRIPTION = 'dMelodies dataset. Algorithmically generated 2-bar melodies. Each datapoint is a 2-bar melody which' \
+              ' plays arpreggios on the standard I-IV-V-I cadence chord pattern. The dataset has 9 independent ' \
+              'latent factors of variation: root, octave, scale, rhythm bar 1, rhythm bar 2, arpeggiation direction ' \
+              'for each of the 4 chords. All possible values of the latent factors are present.'
+VERSION_NUM = 1
+AUTHORS = 'ashis.pati@gatech.edu, siddharth.gururani@gatech.edu, alexander.lerch@gatech.edu'
